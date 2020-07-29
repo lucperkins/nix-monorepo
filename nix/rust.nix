1fb6985215aa276299c5a3869405125a194226f0
Luc Perkins <luc@blockfi.com>
2020-07-28 23:41:44 -0700

    Add inline docs to rust.nix

--- nix/rust.nix
+++ nix/rust.nix
@@ -1,14 +1,21 @@
 let
+  # Pinned repos
   sources = import ./sources.nix;
+
+  # Nix overlays from Mozilla
   mozillaOverlay = import sources.nixpkgs-mozilla;
+
+  # Apply the Mozilla overlay to get Rust-specific Nix stuff
   pkgs = import sources.nixpkgs {
     overlays = [
       mozillaOverlay
     ];
   };
 
+  # Load Naersk from the Niv sources
   naersk = callPackage sources.naersk {};
 
+  # Utilities
   inherit (pkgs) callPackage rustChannelOf;
 
   # Provide a version of Rust specified in the rust-toolchain file
@@ -17,18 +24,22 @@ let
       rustToolchain = toolchain;
     }).rust;
 
+  # Build the Rust project in the specified directory
   buildRustBin = srcs:
     naersk.buildPackage srcs;
-in {
-  pkgs = pkgs;
 
-  shell = toolchain:
+  # Provide a Rust shell based on a rust-toolchain file
+  rustShell = toolchain:
     pkgs.mkShell {
       buildInputs = with pkgs; [
+        cargo-edit
         cargo-watch
+        git
         (rust toolchain)
       ];
     };
+in {
+  shell = rustShell;
   
   build = buildRustBin;
 }
