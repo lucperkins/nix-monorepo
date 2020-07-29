15ef2dc0fbab03a8cbadf93cf86c128693ec1827
Luc Perkins <luc@blockfi.com>
2020-07-28 23:27:03 -0700

    Remove rust target directory from Git

--- rust/target/debug/deps/hello-5a58bce3c95a9cf5.d
+++
@@ -1,5 +0,0 @@
-/Users/lucperkins/workspace/nix/nix-monorepo/rust/target/debug/deps/hello-5a58bce3c95a9cf5.rmeta: src/main.rs
-
-/Users/lucperkins/workspace/nix/nix-monorepo/rust/target/debug/deps/hello-5a58bce3c95a9cf5.d: src/main.rs
-
-src/main.rs:
